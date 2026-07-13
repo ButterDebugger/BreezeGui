b708e3a262c6db52e22ecbc476f2b5df7a99d4d0
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2026-07-13 19:59:05 -0400

    Moved the TUI to its own module

---
+++ breeze-tui/src/lib.rs
@@ -0,0 +1,145 @@
+use crate::config::Config;
+use console::style;
+
+mod config;
+mod pages;
+mod utils;
+
+#[derive(Debug, Default, Clone, strum_macros::Display)]
+enum Page {
+    #[default]
+    #[strum(to_string = "Home")]
+    Home,
+    #[strum(to_string = "Mods")]
+    Mods,
+    #[strum(to_string = "Mods List")]
+    ModsList,
+    #[strum(to_string = "Edit Mod")]
+    EditMod(String),
+    #[strum(to_string = "Modpacks")]
+    Modpacks,
+    #[strum(to_string = "Modpacks List")]
+    ModpacksList,
+    #[strum(to_string = "Manage Modpack")]
+    ManageModpack(String),
+    #[strum(to_string = "Add Branch")]
+    AddBranch(String),
+    #[strum(to_string = "Branch List")]
+    BranchList(String),
+    #[strum(to_string = "Manage Branch")]
+    ManageBranch(String, String),
+    #[strum(to_string = "Create Modpack")]
+    CreateModpack,
+    #[strum(to_string = "Installations")]
+    Installations,
+}
+
+#[derive(Debug, Clone)]
+pub struct App {
+    config: Config,
+    path: Vec<Page>,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        // Get the config
+        let config = if let Some(loaded_config) = Config::load_from_disk() {
+            loaded_config
+        } else {
+            Config::create_config_prompt()
+        };
+
+        // Initialize and save the config
+        config.clone().init();
+        config.clone().save();
+
+        // Return the app
+        Self {
+            config,
+            path: vec![Page::default()],
+        }
+    }
+}
+
+impl App {
+    /// Extends the path to go to the inputted page
+    fn goto(&mut self, page: Page) {
+        self.path.push(page);
+    }
+
+    /// Goes back to the previous page
+    ///
+    /// Should be used when the user backs out of the current page
+    fn go_back(&mut self) {
+        self.path.pop();
+    }
+
+    /// Returns to the home page
+    ///
+    /// Should be used after an action is completed
+    fn return_home(&mut self) {
+        self.path.clear();
+
+        self.path.push(Page::Home);
+    }
+
+    /// Exits out of the app loop
+    fn exit(&mut self) {
+        self.path.clear();
+    }
+
+    /// Starts the app loop
+    pub async fn run(&mut self) {
+        loop {
+            let _ = clearscreen::clear();
+
+            println!();
+
+            // Get current page
+            let current_page = self.path.last();
+
+            if current_page.is_none() {
+                // If there is no path, break out of the app
+                break;
+            }
+
+            let current_page = current_page.unwrap();
+
+            // Output full path
+            for i in 0..self.path.len() {
+                let mut styled_page = style(&self.path[i]);
+
+                if i != self.path.len() - 1 {
+                    styled_page = styled_page.dim();
+                }
+
+                print!("{} {} ", style("▶").blue(), styled_page);
+            }
+
+            println!();
+            println!();
+
+            // Output the current menu
+            match &current_page {
+                Page::Home => self.home_cli(),
+                Page::Mods => self.mods_cli(),
+                Page::ModsList => self.mods_list_cli(),
+                Page::EditMod(mod_name) => self.edit_mod_cli(mod_name.to_string()),
+                Page::Modpacks => self.modpacks_cli(),
+                Page::CreateModpack => self.create_modpack_cli(),
+                Page::ModpacksList => self.modpacks_list_cli(),
+                Page::AddBranch(modpack_name) => {
+                    self.add_branch_cli(modpack_name.to_string()).await
+                }
+                Page::ManageModpack(modpack_name) => {
+                    self.edit_modpack_cli(modpack_name.to_string()).await
+                }
+                Page::BranchList(modpack_name) => self.branch_list_cli(modpack_name.to_string()),
+                Page::ManageBranch(modpack_name, branch_name) => {
+                    self.manage_branch_cli(modpack_name.to_string(), branch_name.to_string())
+                }
+                Page::Installations => self.installations_cli().await,
+            }
+        }
+    }
+}
